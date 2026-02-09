d4aceb1bbdc1f28e4435d331187cfe905d7f0719
Taweng1938274 <dolor.hb@obmontessori.edu.ph>
2026-02-09 18:57:18 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,29 @@
+from pyscript import display, document
+def acc_make(s):
+    document.getElementById('out').innerHTML= " "
+    un=document.getElementById('un').value
+    ps=document.getElementById('ps').value
+    unlen=len(un)
+    pslen=len(ps)
+    if unlen>6:
+        if pslen
+    else:
+        display(f'Username must be more than 6', target-'out')
+
+        
+
+
+"""subj=["English", "Math", "Science", "SS", "Filpino"]
+    for sub in subj:
+        display(f'{sub}', target='out')
+    o=1
+    while co<50:
+        display(co)
+        co+=2
+    while true:
+        print("YO", target="out")
+    for i in range(1,100,2):
+        if i == 9:
+            continue
+        display(i, target="out")"""
+    
